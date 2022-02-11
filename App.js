e353b3b2a21cbd4b28b6819576f3be83eba914dc
Manuj Gogoi <manujgogoi@gmail.com>
2022-02-11 09:25:38 +0530

    A layout is created for layout-01

--- App.js
+++ App.js
@@ -1,19 +1,97 @@
-import { View, Text, StyleSheet } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, Platform } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
-const App = () => {
+export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-    </View>
+    <SafeAreaView style={styles.body}>
+      <View style={styles.row1}>
+        <View style={styles.row1Col1}>
+          <Text>1</Text>
+        </View>
+        <View style={styles.row1Col2}>
+          <Text>2</Text>
+        </View>
+        <View style={styles.row1Col3}>
+          <Text>3</Text>
+        </View>
+      </View>
+      <View style={styles.row2}>
+        <Text>4</Text>
+      </View>
+      <View style={styles.row3}>
+        <Text>5</Text>
+      </View>
+      <View style={styles.row4}>
+        <View style={styles.row4Col1}>
+          <Text>6</Text>
+        </View>
+        <View style={styles.row4Col2}>
+          <Text>7</Text>
+        </View>
+      </View>
+      <StatusBar style="light" />
+    </SafeAreaView>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  container: {
+  body: {
     flex: 1,
+    flexDirection: "column",
+    backgroundColor: "#229999",
+    alignItems: "stretch",
+    justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? 25 : 0,
+  },
+  row1: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "#9988aa",
+  },
+  row1Col1: {
+    flex: 1,
+    backgroundColor: "#03ecfc",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  row1Col2: {
+    flex: 2,
+    backgroundColor: "#c203fc",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  row1Col3: {
+    flex: 3,
+    backgroundColor: "#f8fc03",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  row2: {
+    flex: 1,
+    backgroundColor: "#fc030f",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  row3: {
+    flex: 1,
+    backgroundColor: "#03fc0b",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  row4: {
+    flex: 5,
+    flexDirection: "row",
+  },
+  row4Col1: {
+    flex: 1,
+    backgroundColor: "#eee",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  row4Col2: {
+    flex: 1,
+    backgroundColor: "#3333ff",
     alignItems: "center",
     justifyContent: "center",
   },
 });
-
-export default App;
