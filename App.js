79d6455b847279349f55f6238238f7b67f177052
Manuj Gogoi <manujgogoi@gmail.com>
2022-02-12 23:18:17 +0530

    Modal practice

--- App.js
+++ App.js
@@ -1,23 +1,85 @@
-import { View, Text } from "react-native";
-import { ToastProvider } from "react-native-toast-notifications";
-import Temp from "./src/components/sample/Temp";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  Platform,
+  Pressable,
+  Modal,
+} from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 
 const App = () => {
+  const [modalVisible, setModalVisible] = useState(false);
   return (
-    <ToastProvider
-      renderType={{
-        custom: (toast) => (
-          <View
-            style={{ backgroundColor: "#666", padding: 10, borderRadius: 10 }}
-          >
-            <Text style={{ fontSize: 16, color: "#fff" }}>{toast.message}</Text>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="dark" />
+      <View>
+        <Text style={styles.text}>Hello</Text>
+      </View>
+      <Pressable style={styles.button} onPress={() => setModalVisible(true)}>
+        <Text style={styles.buttonText}>Click Me!</Text>
+      </Pressable>
+
+      {/* Modal */}
+      <Modal
+        visible={modalVisible}
+        transparent
+        animationType="slide"
+        hardwareAccelerated
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.modalBackdrop}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalTitle}>Hello I am a Modal!!</Text>
+            <Pressable
+              style={styles.button}
+              onPress={() => setModalVisible(false)}
+            >
+              <Text style={styles.buttonText}>Close</Text>
+            </Pressable>
           </View>
-        ),
-      }}
-    >
-      <Temp />
-    </ToastProvider>
+        </View>
+      </Modal>
+      {/* Modal End */}
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? 25 : 0,
+    backgroundColor: "#63e0d4",
+  },
+  text: {
+    color: "#2f2f2f",
+    fontSize: 66,
+  },
+  button: {
+    backgroundColor: "#313ab0",
+    alignItems: "center",
+    paddingVertical: 10,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+    textTransform: "uppercase",
+  },
+  modalBackdrop: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#000000aa",
+  },
+  modalView: {
+    backgroundColor: "pink",
+    width: 300,
+    height: 400,
+    padding: 10,
+    borderRadius: 10,
+  },
+});
 
 export default App;
